729e5b8ed6f7feb24722a39fe120d0980e6c1dad
Peter Bowyer <peter@mapledesign.co.uk>
2015-11-26 10:26:02 +0000

    Watch all forms added to the page
    When forms are added via AJAX, the original library can't detect them. This modification does, which reduces some features (you are responsible for making the form fields have the correct names etc) and focuses the library back on its task: being a lightweight replacement for MailChimp's own AJAX-submit feature.

--- src/jquery.ajaxchimp.js
+++ src/jquery.ajaxchimp.js
@@ -1,34 +1,37 @@
 /*!
-Mailchimp Ajax Submit
-jQuery Plugin
-Author: Siddharth Doshi
+ Mailchimp Ajax Submit
+ jQuery Plugin
+ Author: Siddharth Doshi
+ Modified by Peter Bowyer <peter@mapledesign.co.uk> to handle forms being added to the page
 
-Use:
-===
-$('#form_id').ajaxChimp(options);
+ Based off the 2.0-dev branch
 
-- Form should have one <input> element with attribute 'type=email'
-- Error and success messages will be displayed in elements within the form
-    found by errorSelector, successSelector respectively
-- The init function returns a deferred object onto which callbacks can be
-    attached similar to the jqXhr object returned by $.ajax (done/fail/always)
-- All options are optional :).
+ Use:
+ ===
+ $(body).ajaxChimp(options);
 
-Options:
-=======
-options = {
-    language: 'en',
-    errorSelector: '.error-message',
-    successSelector: '.success-message',
-    url: 'http://blahblah.us1.list-manage.com/subscribe/post?u=5afsdhfuhdsiufdba6f8802&id=4djhfdsh99f',
-    token: 'anti-bot token from mailchimp form'
-}
+ - Works for all forms that appear on the page, matching the selector
+ - Error and success messages will be displayed in elements within the form
+ found by errorSelector, successSelector respectively
+ - The init function returns a deferred object onto which callbacks can be
+ attached similar to the jqXhr object returned by $.ajax (done/fail/always)
+ - All options are optional :).
 
-Notes:
-=====
-To get the mailchimp JSONP url (undocumented), change 'post?' to 'post-json?' and add '&c=?' to the end.
-For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsiufdba6f8802&id=4djhfdsh99f&c=?',
-*/
+ Options:
+ =======
+ options = {
+ language: 'en',
+ errorSelector: '.error-message',
+ successSelector: '.success-message',
+ token: 'anti-bot token from mailchimp form',
+ target: '.my-mailchimp-forms' // Used to scope the listener
+ }
+
+ Notes:
+ =====
+ To get the mailchimp JSONP url (undocumented), change 'post?' to 'post-json?' and add '&c=?' to the end.
+ For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsiufdba6f8802&id=4djhfdsh99f&c=?',
+ */
 
 ;(function (factory) {
     if (typeof define === 'function' && define.amd) {
@@ -76,6 +79,7 @@ For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsi
             errorSelector: '#mce-error-response',
             successSelector: '#mce-success-response',
             token: null,
+            target: '.my-mailchimp-forms'
         },
         successMessage: 'Please confirm by clicking on the link we just sent to ',
         submitMessage: 'Submitting...',
@@ -87,8 +91,8 @@ For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsi
     $.ajaxChimp.getTranslation = function(str, language, string_key) {
         if(
             $.ajaxChimp.all_translations[language] &&
-            $.ajaxChimp.all_translations[language][string_key]
-        ) {
+                $.ajaxChimp.all_translations[language][string_key]
+            ) {
             var regexPattern = $.ajaxChimp.regexPatterns[string_key];
             var translation = $.ajaxChimp.all_translations[language][string_key];
             if ($.type(regexPattern) === 'regexp') {
@@ -125,120 +129,86 @@ For e.g. 'http://blahblah.us1.list-manage.com/subscribe/post-json?u=5afsdhfuhdsi
             $.ajaxChimp.defaultTranslations,
             $.ajaxChimp.translations
         );
+        var deferred = new $.Deferred();
 
-        var deferreds = $(this).map(function(i, elem) {
-            var deferred = new $.Deferred();
-            var form = $(elem);
-            form.attr('novalidate', 'true');
+        // Create settings object from default and passed in options
+        var settings = $.extend($.ajaxChimp.defaultOptions, options);
 
-            // Create settings object from default and passed in options
-            var settings = $.extend({
-                url: form.attr('action')
-            }, $.ajaxChimp.defaultOptions, options);
 
-            var email = form.find('input[type=email]');
-            email.attr('name', 'EMAIL');
+        $(this).on('submit', settings.target, function (event) {
+            event.preventDefault();
 
-            // Add or update token input if anti-bot token passed in from settings
-            if (settings.token) {
-                var tokenInput = form.find('input[name="' + settings.token + '"]');
-                if (tokenInput.length === 0) {
-                    var tokenDiv = $('<div/>', {
-                        style: 'position: absolute; left: -5000px;',
-                    });
-                    tokenDiv.append($('<input>', {
-                        type: 'text',
-                        name: settings.token,
-                        tabindex: '-1',
-                        value: ''
-                    }));
-                    form.append(tokenDiv);
-                } else {
-                    tokenInput.attr('name', settings.token);
-                }
-            }
+            var form = $(event.target);
 
-            // Add hidden submit element if none present
-            if (form.find('input[type="submit"]').length === 0) {
-                var submitInput = $('<input>', {
-                    type: 'submit',
-                    style: 'display: none;',
-                });
-                form.append(submitInput);
-            }
+            // Convert url to jsonp url
+            var url = form.attr('action').replace('/post?', '/post-json?').concat('&c=?');;
 
             var error_div = form.find(settings.errorSelector);
             var success_div = form.find(settings.successSelector);
+            var email = form.find('input[type="email"]');
 
-            // Convert url to jsonp url
-            var url = settings.url.replace('/post?', '/post-json?').concat('&c=?');
-
-            form.on('submit', function (event) {
-                event.preventDefault();
-
-                var msg;
-                var request_data = {};
-                $.each(form.serializeArray(), function (index, item) {
-                    request_data[item.name] = item.value;
-                });
-
-                $.ajax({
-                    url: url,
-                    data: request_data,
-                    dataType: 'jsonp'
-                }).done(function (data, textStatus, jqXHR) {
-                    if (data.result === 'success') {
-                        email.removeClass('error').addClass('valid');
-                        if (success_div.length !== 0) {
-                            msg = $.ajaxChimp.getTranslation(
-                                ($.ajaxChimp.successMessage + request_data.EMAIL),
-                                settings.language,
-                                'success'
-                            );
-                            error_div.text('').hide();
-                            success_div.text(msg).show(500);
-                        }
-                    } else{
-                        email.removeClass('valid').addClass('error');
-                        if (error_div.length !== 0) {
-                            try {
-                                var parts = data.msg.split(' - ', 2);
-                                if (parts[1] === undefined) {
-                                    msg = data.msg;
-                                } else {
-                                    msg = parts[1];
-                                }
-                            }
-                            catch (e) {
-                                msg = data.msg;
-                            }
-                            msg = $.ajaxChimp.getTranslation(msg, settings.language, 'error');
-                            success_div.text('').hide();
-                            error_div.text(msg).show(500);
-                        }
-                    }
-                    deferred.resolve(data, textStatus, jqXHR, form);
-                }).fail(function (jqXHR, textStatus, errorThrown) {
-                    deferred.reject(jqXHR, textStatus, errorThrown, form);
-                });
-
-                // Display submit message
-                email.removeClass('valid error');
-                if (success_div.length !== 0) {
-                    var submitMsg = $.ajaxChimp.getTranslation(
-                        $.ajaxChimp.submitMessage,
-                        settings.language,
-                        'submit'
-                    );
-                    error_div.text('').hide();
-                    success_div.text(submitMsg).show(500);
-                }
+            var msg;
+            var request_data = {};
+            $.each(form.serializeArray(), function (index, item) {
+                request_data[item.name] = item.value;
             });
-            return deferred;
+            if (settings.token !== '') {
+                request_data[settings.token] = '';
+            }
+
+            $.ajax({
+                url: url,
+                data: request_data,
+                dataType: 'jsonp'
+            }).done(function (data, textStatus, jqXHR) {
+                if (data.result === 'success') {
+                    email.removeClass('error').addClass('valid');
+                    if (success_div.length !== 0) {
+                        msg = $.ajaxChimp.getTranslation(
+                            ($.ajaxChimp.successMessage + request_data.EMAIL),
+                            settings.language,
+                            'success'
+                        );
+                        error_div.text('').hide();
+                        success_div.text(msg).show(500);
+                    }
+                } else{
+                    email.removeClass('valid').addClass('error');
+                    if (error_div.length !== 0) {
+                        try {
+                            var parts = data.msg.split(' - ', 2);
+                            if (parts[1] === undefined) {
+                                msg = data.msg;
+                            } else {
+                                msg = parts[1];
+                            }
+                        }
+                        catch (e) {
+                            msg = data.msg;
+                        }
+                        msg = $.ajaxChimp.getTranslation(msg, settings.language, 'error');
+                        success_div.text('').hide();
+                        error_div.text(msg).show(500);
+                    }
+                }
+                deferred.resolve(data, textStatus, jqXHR, form);
+            }).fail(function (jqXHR, textStatus, errorThrown) {
+                deferred.reject(jqXHR, textStatus, errorThrown, form);
+            });
+
+            // Display submit message
+            email.removeClass('valid error');
+            if (success_div.length !== 0) {
+                var submitMsg = $.ajaxChimp.getTranslation(
+                    $.ajaxChimp.submitMessage,
+                    settings.language,
+                    'submit'
+                );
+                error_div.text('').hide();
+                success_div.text(submitMsg).show(500);
+            }
         });
-        if (deferreds.length === 1){
-            return deferreds[0];
-        }
-        return deferreds;
+
+        return deferred;
     };
 }));
